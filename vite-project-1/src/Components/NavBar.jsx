c4ad3dfbca802e22857c250c5e3dccc2df466755
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-04 16:33:56 -0700

    Started routing for all tabs

--- vite-project-1/src/Components/NavBar.jsx
+++ vite-project-1/src/Components/NavBar.jsx
@@ -2,13 +2,25 @@ import React from "react";
 
 function NavBar(props) {
   return (
-  <div>
-    <Projects/>
-    <Experience/>
-    <AboutMe/>
-    <Contact/>
-  </div>
+    <nav className="nav">
+      <a href="/" className="site-title">The Colin Portfolio</a>
+      <ul>
+        <li>
+          <a href="Projects">Projects</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+        <li>
+          <a href="About">About</a>
+        </li>
+        <li>
+          <a href="Contact">Contact</a>
+        </li>
+      </ul>
+    </nav >
 
-)}
+  );
+}
 
-export default NavBar
+export default NavBar;
