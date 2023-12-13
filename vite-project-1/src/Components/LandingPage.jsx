30e8690b8468a970b2c2f033bfc27cfaedff5e8f
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-13 12:56:24 -0700

    Added in final project and description to the landing page.

--- vite-project-1/src/Components/LandingPage.jsx
+++ vite-project-1/src/Components/LandingPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import '../styles/LandingPage.css';
+
 
 function LandingPage(props) {
   return (
@@ -7,11 +9,14 @@ function LandingPage(props) {
         <introductionimage>IMAGE PLACEHOLDER</introductionimage>
         <introductiontext>Explain a little from what my background is</introductiontext>
       </div>
-      <div>
-        <div>Final Bootcamp Project Description</div>
-        <div>FINAL PROJECT IMAGE PLACEHOLDER WITH LINK</div>
+
+        <div className="project-image-description">
+          <div>A web application for restaurants that allows users to reserve specific seating while booking a reservation. A user can register and log in. A user is able to store their favorites between sessions. A user will also recieve texts that update their delivery status.</div>
+          <a href="https://github.com/codingMadeSimple/Charcoal-Cabernet">
+            <img className="top-padding" src="https://raw.githubusercontent.com/NeonWaffles222/Charcoal-Cabernet/master/docs/Top%20of%20Homepage.png" alt="Charcoal and Cabernet Screenshot Placeholder" ></img>
+          </a>
+        </div>
         <div>Why the gap on github? Because I got Married!</div>
-      </div>
 
 
     </div>
