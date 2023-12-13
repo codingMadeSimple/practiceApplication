38120a86bc5b49f511f09bededa595af2fd5e354
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-12 17:28:40 -0700

    Created and started building out structure to the landing page.

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NavBar from './Components/NavBar'
-import "./styles/NavBar.css"
-
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+import "./styles/NavBar.css";
+import LandingPage from './Components/LandingPage';
 
 
 function App() {
 
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <LandingPage />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ vite-project-1/src/Components/LandingPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function LandingPage(props) {
+  return (
+    <div>
+      <div>
+        <introductionimage>IMAGE PLACEHOLDER</introductionimage>
+        <introductiontext>Explain a little from what my background is</introductiontext>
+      </div>
+      <div>
+      <div>Final Bootcamp Project Description</div>
+      <div>FINAL PROJECT IMAGE PLACEHOLDER WITH LINK</div>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default LandingPage;
