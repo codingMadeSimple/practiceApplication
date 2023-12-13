1476baa13c4008cccd9dbdc90bf0fbcaf8e9d54b
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-12 17:06:43 -0700

    Stickied nav bar to top and fixed incorrect styling.

--- vite-project-1/src/Components/NavBar.jsx
+++ vite-project-1/src/Components/NavBar.jsx
@@ -3,23 +3,24 @@ import React from "react";
 function NavBar(props) {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">The Colin Portfolio</a>
-      <ul>
-        <li>
-          <a href="Projects">Projects</a>
-        </li>
-        <li>
-          <a href="About">About</a>
-        </li>
-        <li>
-          <a href="About">About</a>
-        </li>
-        <li>
-          <a href="Contact">Contact</a>
-        </li>
-      </ul>
-    </nav >
-
+      <div className="container">
+        <a href="/" className="site-title">The Colin Portfolio</a>
+        <ul>
+          <li>
+            <a href="Projects">Projects</a>
+          </li>
+          <li>
+            <a href="About">About</a>
+          </li>
+          <li>
+            <a href="About">About</a>
+          </li>
+          <li>
+            <a href="Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
