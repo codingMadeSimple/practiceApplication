bd07b275bbdc280fc03a5ffc128caecc04fbd090
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-15 15:06:00 -0700

    Changed css to scss

--- vite-project-1/src/App.jsx
+++ vite-project-1/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
-import "./styles/NavBar.css";
+import './App.scss';
+import "./styles/NavBar.scss";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import LandingPage from './Components/LandingPage';
