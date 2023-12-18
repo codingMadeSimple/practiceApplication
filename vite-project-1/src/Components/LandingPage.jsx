10382df696d66e4ebceeee38395b69d06cd01620
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-18 10:37:23 -0700

    Added short description>'

--- vite-project-1/src/Components/LandingPage.jsx
+++ vite-project-1/src/Components/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage(props) {
     <div>
       <div>
         <introductionimage>IMAGE PLACEHOLDER</introductionimage>
-        <introductiontext>Explain a little from what my background is</introductiontext>
+        <introductiontext>I am a LightHouseLabs Web Development Graduate and a Full Stack Web Developer who leans slightly towards frontend. From Climbing to Coding and everything in-between. I would love to meet and collaborate with others in the industry to grow my knowledge and abilities.</introductiontext>
       </div>
 
         <div className="project-image-description">
