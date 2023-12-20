4decc3488b756ff1b21c359b256a11ad3073096d
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-20 13:26:58 -0700

    Changed colors and some styling for navigation tabs so that the data shows up either centered on the page or in two columns.

--- vite-project-1/src/Components/Contact.jsx
+++ vite-project-1/src/Components/Contact.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import "../styles/Contact.scss"
+
 
 function Contact(props) {
   return (
     <div className="page-margin">
-      <div>
+      <div className="center-text">
         <h2>Socials</h2>
         <div>
-          <a href="https://github.com/codingMadeSimple">Github Profile
-          </a>
-        </div>
-        <div>
-          <a href="https://www.linkedin.com/in/colin-spangelo/">Linked In Profile
-          </a>
+          <div>
+            <a href="https://github.com/codingMadeSimple">Github Profile
+            </a>
+          </div>
+          <div>
+            <a href="https://www.linkedin.com/in/colin-spangelo/">Linked In Profile
+            </a>
+          </div>
         </div>
       </div>
     </div>
