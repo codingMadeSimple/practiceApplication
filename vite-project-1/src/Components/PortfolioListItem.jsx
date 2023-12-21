43e0e79bbeeeacde2de1bcf0e8bad889366867aa
Colin Spangelo <colin.spangelo@gmail.com>
2023-12-21 14:40:11 -0700

    Added Data and collaborators so that you can access the github and linked in of both of my final project partners.

--- vite-project-1/src/Components/PortfolioListItem.jsx
+++ vite-project-1/src/Components/PortfolioListItem.jsx
@@ -9,22 +9,31 @@ function PortfolioListItem(props) {
     <div className="page-margin">
       <div className="portfolio-image-description margin-top">
         <div className="margin-top">
-          <h1 className="space-apart">{props.projectObject.projectName}</h1>
+          <h1 className="space-apart title-color">{props.projectObject.projectName}</h1>
           <div>{props.projectObject.projectDescription}</div>
           <div>
-            <h2>Tech Stack</h2>
+            <h1 className="title-color">Tech Stack</h1>
             <>
-              <h4>Front End</h4>
-              <div>{props.projectObject.frontendStack}</div>
+              <div><h2 className="text-color">Front End</h2>- {props.projectObject.frontendStack}</div>
             </>
             <>
-              <h4>Back End</h4>
-              <div>{props.projectObject.backendStack}</div>
+              <h2 className="text-color">Back End</h2>- {props.projectObject.backendStack}
             </>
             <>
-              <h4>Api's and Libraries</h4>
-              <div>{props.projectObject.apis}</div>
+              <h2 className="text-color">Api's and Libraries</h2> - {props.projectObject.apis}
             </>
+            <div className="side-by-side">
+              <div className="collaborator-sizing">
+                <h2 className="text-color">Collaborators</h2>{props.projectObject.partners.Steven.name}
+                <a href={props.projectObject.partners.Steven.linkedIn}><div className="link-color">Linked In</div></a>
+                <a href={props.projectObject.partners.Steven.github}><div className="link-color">Github</div></a>
+              </div>
+              <div className="collaborator-sizing">
+                {props.projectObject.partners.Noah.name}
+                <a href={props.projectObject.partners.Noah.linkedIn}><div className="link-color">Linked In</div></a>
+                <a href={props.projectObject.partners.Noah.github}><div className="link-color">Github</div></a>
+              </div>
+            </div>
           </div>
         </div>
         <a href="https://github.com/codingMadeSimple/Charcoal-Cabernet">
